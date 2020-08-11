2861d579aa101ff659b39573f6f35dbd7555ea2c
Ram Charan <ramcharan@Rams-MacBook-Pro.local>
2020-08-12 02:59:44 +0530

    Created Custom Server File to handle Default Path for routing

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,11 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   console.log(appContext)
+//   //const appProps = await App.getInitialProps(appContext);
 
+//   return { }
+// }
 export default MyApp
